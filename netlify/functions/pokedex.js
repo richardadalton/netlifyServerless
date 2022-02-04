ed4ac53d8cb3678e596444bbcb4cd7c0512a7933
Richard Dalton <richard@devjoy.com>
2022-02-04 09:38:16 +0000

    Deploy functions

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch';
+
+exports.handler = async function () {
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+
+    const response = await fetch(POKE_API)
+    const data = await response.json()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    }
+}
