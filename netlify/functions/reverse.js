e8b2908f05ac932ca6905beaec9634689416f925
Richard Dalton <richard@devjoy.com>
2023-09-08 09:05:26 +0100

    Add POST to reverse function

---
+++ netlify/functions/reverse.js
@@ -0,0 +1,9 @@
+exports.handler = async function(event, context) {
+    console.log({event}, {context})
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Reverse!"
+        })
+    }
+}
